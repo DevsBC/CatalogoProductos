f95ee9b2f851259ddc273e570f199b600bce5dc0
ltrbomb10 <ltrbomb10@ltrbomb10>
2018-03-04 18:37:17 -0700

    #8 -> Quinta version, casi version final

--- src/productos/Producto.java
+++ src/productos/Producto.java
@@ -10,31 +10,47 @@ import java.util.*;
 
 /**
  *
- * @author ltrbomb10
+ * @authors:
+ * Juan Carlos Aranda,
+ * Luis Angel Aguila, 
+ * Carlos Eduardo Camacho,
+ * Jose Adolfo Armenta.
  */
 public class Producto {
     
-    private  String codigo;
-    private  String descripcion;
-    private  double precio;
+    /* Variables de instancia */
+    private String codigo;
+    private String descripcion;
+    private double precio;
     
+    /* Constructor vacio para la consistencia de los datos */
     Producto() {
         setCodigo("");
         setDescripcion("");
         setPrecio(0);
     }
     
+    /* Constructor con parametros por si acaso */
     Producto(String codigo, String descripcion, double precio) {
         setCodigo(codigo);
         setDescripcion(descripcion);
         setPrecio(precio);
     }
     
+    /* variable de entrada al teclado */
     private static Scanner in =  new Scanner(System.in);
+    
+    /* Objetos de manejo de archivos */
     private static File file = new File("productos.dat");
     private static DataOutputStream dos;
     private static DataInputStream dis; 
+    
+    /* Array del objeto Producto */
     private static ArrayList<Producto> productos = new ArrayList<>();
+    
+    /* Objeto Producto para rellenar los datos */
+    private static Producto p = new Producto();
+    
     
     public String getCodigo() {
         return codigo;
@@ -63,102 +79,103 @@ public class Producto {
             this.precio = precio;
     }
     
+    
+    /* Metodo para pedir datos al usuario y escribirlos al archivo productos.dat */
     public static void capturarProductos(){
-        Producto p = new Producto();
-        int count = 0;
+        int contadorDeProductos = 0;
+        /* Variable para confirmar que se han ingresado todos los datos */
         String exit= "";
+        
+        /* Variable auxiliar para continuar ciclo */
         boolean continuar = true;
-        do{
         
-        try {     
-            file.createNewFile();
-             
-            dos = new DataOutputStream(
-                  new BufferedOutputStream(
-                  new FileOutputStream(file)));
+        do{  
+            try {     
+                file.createNewFile();
+                
+             /* Abrir conexion con el archivo */
+                dos = new DataOutputStream(
+                      new BufferedOutputStream(
+                      new FileOutputStream(file)));
             
-            while(!exit.equalsIgnoreCase("S")){
-                System.out.print("Ingrese el codigo: ");
-                String codigo = in.next();
-                p.setCodigo(codigo);
-                dos.writeUTF(p.getCodigo());
+                while(!exit.equalsIgnoreCase("S")){
+                    System.out.print("Ingrese el codigo: ");
+                    String codigo = in.next();
+                    p.setCodigo(codigo);
+                    dos.writeUTF(p.getCodigo()); 
                 
-                System.out.print("Ingrese la descripcion: ");
-                String descripcion = in.next();
-                p.setDescripcion(descripcion);
-                dos.writeUTF(p.getDescripcion());
+                    System.out.print("Ingrese la descripcion: ");
+                    String descripcion = in.next();
+                    p.setDescripcion(descripcion);
+                    dos.writeUTF(p.getDescripcion()); // escribe en archivo
                 
-                System.out.print("Ingrese el precio: ");
-                double precio = in.nextDouble();
-                p.setPrecio(precio);
-                dos.writeDouble(p.getPrecio());
+                    System.out.print("Ingrese el precio: ");
+                    double precio = in.nextDouble();
+                    p.setPrecio(precio);
+                    dos.writeDouble(p.getPrecio()); // escribe en archivo
                 
-                count++;
+                    contadorDeProductos++;
                 
-                System.out.print("Ha terminado?(S/N):");
-                exit = in.next();      
+                    System.out.print("Ha terminado?(S/N):");
+                    exit = in.next();
+                    System.out.println("");
+                }
+                System.out.println("Hay " + contadorDeProductos + " productos");
+            
+                continuar = false;
+                
+                dos.close();
+            } catch(IOException e) {
+                System.out.println("Hubo un error al tratar el archivo: " + e);
+            } catch(InputMismatchException e) {
+                System.err.println("Ingrese un valor valido\n " + e);
+                in.next(); 
+                System.out.println("Introducir datos de nuevo\n");
             }
-        
-            System.out.println("Hemos salido");
-            System.out.println("Hay " + count + " productos");
-            
-            continuar = false;
-            dos.close();
-        } catch(IOException e) {
-            System.out.println("Hubo un error mi compa " + e);
-        } catch(InputMismatchException e) {
-            System.err.println("Ingrese un valor valido " + e);
-            in.next(); 
-            System.out.println("Introducir datos de nuevo");
-        }
         }while(continuar);
     }
     
-    
+    /* Metodo para leer del archivo para pasarlo al programa */
     public static void leerProductos() {
-        Producto p2 = new Producto();
-        String codigo;
-        String descripcion;
-        double precio;
         try {
             file.createNewFile();
             
+            /* abre conexion con el archivo */
             dis = new DataInputStream(
-            new BufferedInputStream(
-            new FileInputStream(file)));
+                  new BufferedInputStream(
+                  new FileInputStream(file)));
             
-            System.out.println("pendejo");
-            
-            while(dis.available() > 0) {
-                System.out.println("entro");
+            while(dis.available() > 0) {   
+                p = new Producto(); // crea un objeto nuevo en cada iteracion
+                String codigo = dis.readUTF(); // lee del archivo
+                p.setCodigo(codigo);
                 
-                codigo = dis.readUTF();
-                p2.setCodigo(codigo);
-                descripcion = dis.readUTF();
-                p2.setDescripcion(descripcion);
-                precio = dis.readDouble();
-                p2.setPrecio(precio);
+                String descripcion = dis.readUTF(); // lee del archivo
+                p.setDescripcion(descripcion);
                 
-                System.out.println("entromas");
-                
-                productos.add(p2);    
+                double precio = dis.readDouble(); // lee del archivo
+                p.setPrecio(precio);
+        
+                productos.add(p); // agrega el objeto actual al arrayList
             }
             dis.close();
         } catch (IOException e) {
-            System.out.println("No se armo compa " + e);
+            System.out.println("Error al tratar el archivo: " + e);
         }
     }
     
     public static void mostrarProductos() {
+        /* Metodo para recorrer el ArrayList */
         productos.stream().forEach((producto) -> {
-            System.out.println("\n" + producto);
+            System.out.println(producto);
         });
     }
     
+    /* Formato del objeto */
     @Override
     public String toString() {
-       return "Codigo: " + codigo 
+       return "\nCodigo: " + codigo 
                + "\nDescripcion: " + descripcion 
-               + "\nPrecio: " + precio; 
+               + "\nPrecio: " + precio + "\n";
     }
 }
